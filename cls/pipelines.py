87417829495ddc059a8544e94e63f439bfa8b986
Drew <tke578@gmail.com>
2018-03-08 09:30:04 -0800

    Getting somewhere

--- cls/pipelines.py
+++ cls/pipelines.py
@@ -31,7 +31,8 @@ class MongoDBPipeline(object):
         )
         db = connection[settings['MONGODB_DB']]
         self.collection = db[settings['MONGODB_COLLECTION']]
-        self.posts_with_status_new = list(self.collection.find({"post_status": "new", "uuid":{"$ne":None}}))
+        uuids_collection = list(self.collection.find({"post_status": "new"}, { "uuid": 1 }))
+        if len(uuids_collection) > 0 : self.list_of_uuids = list(map(lambda x: x['uuid'], uuids_collection))
         log.msg("MongoDBPipeline has been initialize!")
         
     def process_item(self, item, spider):
@@ -42,10 +43,11 @@ class MongoDBPipeline(object):
                 valid = False
                 raise DropItem("Missing {0}!".format(data))
 
-        if self.is_unique(item) is False:
-            valid = False
-            raise DropItem("Scraped post already exists with no changes: %s"  % data)
-
+        
+        if self.list_of_uuids:
+            if self.is_unique(item) is False:
+                valid = False
+                raise DropItem("Scraped post already exists with no changes: %s"  % data)
 
         if valid:
             self.collection.insert(dict(item))
@@ -55,22 +57,26 @@ class MongoDBPipeline(object):
 
     
     def is_unique(self, attribute):
-        if len(self.posts_with_status_new) > 0:
-            for index, post in enumerate(self.posts_with_status_new):
-                if post["uuid"] == attribute["uuid"]:
-                    del self.posts_with_status_new[index]
-                    return False
-
-    def close_spider(self,spider):
-        if len(self.posts_with_status_new) > 0:
-            for post in self.posts_with_status_new:
+        
+        if attribute["uuid"] in self.list_of_uuids:
+            # code.interact(local=dict(globals(), **locals()))
+            return False
+            # if post["uuid"] in 
+            # for index, post in enumerate(self.posts_with_status_new):
+                # if post["uuid"] == attribute["uuid"]:
                 
-                page = requests.get(post['url'])
-                tree = html.fromstring(page.content)
-                if len(tree.xpath('//div[@class="post-not-found"]/h1/text()'))  == 1:
-                    self.collection.update({ "uuid" : post['uuid']}, { "$set": { "post_status": "not-found", "status_changed": "new Date()"}})
-                else:
-                    code.interact(local=dict(globals(), **locals()))
+                    # del self.posts_with_status_new[index]
+
+    # def close_spider(self,spider):
+        # if len(self.posts_with_status_new) > 0:
+        #     for post in self.posts_with_status_new:
+                
+        #         page = requests.get(post['url'])
+        #         tree = html.fromstring(page.content)
+        #         if len(tree.xpath('//div[@class="post-not-found"]/h1/text()'))  == 1:
+        #             self.collection.update({ "uuid" : post['uuid']}, { "$set": { "post_status": "not-found", "status_changed": "new Date()"}})
+                
+                    
 
         
          # print('Closing {} spider'.format(spider.name))
